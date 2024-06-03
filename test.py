7b53f8ab01b90d2a5530adb76951e42d79c03772
Moshkovskiy Volodomyr <moshkovskiy.v.v@Gmail.com>
2024-06-03 11:28:13 +0300

    Автовход на weblancer

--- test.py
+++ test.py
@@ -1,24 +1,32 @@
 import os
-import re
-import subprocess
 import time
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+global url
+urlWeblancer = "https://www.weblancer.net/"
 
 def test_add_unpacked_extension():
     options = webdriver.ChromeOptions()
 
-    # Укажите путь к директории вашего разархивированного расширения
+    # Путь к плагину и его добавление
     extension_dir_path = os.path.abspath("C:/Users/crm.28540/Desktop/USP_Plugin")
-
     options.add_argument(f"--load-extension={extension_dir_path}")
 
     driver = webdriver.Chrome(options=options)
-    driver.get("https://freelance.habr.com/")
+    driver.get(urlWeblancer)
 
-    # Добавить время ожидания, чтобы увидеть результат
-    time.sleep(10)  # Ждем 10 секунд
+    # Поиск и нажатие на "Вход"
+    element1 = driver.find_element(By.LINK_TEXT, "Вход")
+    element1.click()
+    time.sleep(5)
+
+    # Поиск и нажатие на елемент плагина "Автовход"
+    button = driver.find_element(By.LINK_TEXT, "Автовход") #По тексту
+    #button = driver.find_element(By.ID, "button-auth-extention") # по ID
+    button.click()
+    time.sleep(5)  # Ждем 10 секунд
+
 
     # Закрыть браузер
     driver.quit()
